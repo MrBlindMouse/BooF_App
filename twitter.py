e5a42a1be5c4b541004c4e0227f3e2047dcfc5ce
MrBlindMouse <neljohan1206@gmail.com>
2025-07-03 19:57:53 +0200

    Twitter functions updated

--- twitter.py
+++ twitter.py
@@ -4,18 +4,37 @@ from dotenv import dotenv_values
 import valr
 
 envConfig = dotenv_values(".env")
-xKey = envConfig["X_KEY"]
-xSecret = envConfig["X_SECRET"]
-xToken = envConfig["X_TOKEN"]
-xTokenSecret = envConfig["X_TOKEN_SECRET"]
+xClientID = envConfig["X_CLIENT_ID"]
+xClientSecret = envConfig["X_CLIENT_SECRET"]
+
+def xBearerToken(clientID, clientSecret):
+    url = "https://api.twitter.com/oauth2/token"
+    auth = (client_id, client_secret)
+    data = {"grant_type": "client_credentials"}
+    
+    response = requests.post(url, auth=auth, data=data)
+    if response.status_code != 200:
+        print("Error fetching Bearer Token:")
+        print(response.reason, response.json())
+        return None
+    return response.json().get("access_token")
 
 def xPost(message):
-    auth = OAuth1(xKey, xSecret, xToken, xTokenSecret)
+    token = xBearerToken(xClientID, xClientSecret)
+    if not token:
+        return
+
     url = "https://api.twitter.com/2/tweets"
-    payload = {"text":message}
-    result = requests.post(url, auth=auth, json=payload)
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    payload = {"text": message}
+    
+    result = requests.post(url, headers=headers, json=payload)
     if result.status_code == 201:
         print("X update success!")
+        print(result.content)
     else:
         print("Error posting to X:")
         print(result.reason)
