2083a0f422fec96d88bb5db16ee5191cb002565c
MrBlindMouse <neljohan1206@gmail.com>
2025-07-11 12:52:37 +0200

    Removed "__main__" from twitter.py
     Changes to be committed:
    	modified:   twitter.py

--- twitter.py
+++ twitter.py
@@ -15,8 +15,3 @@ def sendTweet(msg):
     result = client.create_tweet(text=msg)
     print(json.dumps(result.data, indent=4))
     print(json.dumps(result.errors, indent=4))
-
-if __name__ == "__main__":
-    msg = "Crypto Trend Update: SOL Trend:0.95 RSI:61 | AVAX Trend:0.89 RSI:63 | ETH Trend:1.12 RSI:72 | BTC Trend:1.07 RSI:69 | XRP Trend:1.09 RSI:72. The General Market Trend is 1.02. Brought to you by boof-bots.com, and valr.com #CryptoTrends #TradingBot #HODL"
-    print(len(msg))
-    sendTweet(msg)
