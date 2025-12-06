df972fac92003895b2544bc15117f8a6a52619fd
MrBlindMouse <neljohan1206@gmail.com>
2025-12-06 09:53:00 +0200

    Prompt update
    Added '$' to coin names in the prompt

--- valr.py
+++ valr.py
@@ -2301,8 +2301,8 @@ def xUpdate(side, config=Config):
             high = entry
         if entry["trend"] < low["trend"]:
             low = entry
-    low_data = f"The worst performing coin is {low["base"]} with {analysis(low["trend"])}, while the general market has {analysis(generalTrend)}"
-    high_data = f"The best performing coin is {high["base"]} with {analysis(high["trend"])}, while the general market has {analysis(generalTrend)}"
+    low_data = f"The worst performing coin is ${low["base"]} with {analysis(low["trend"])}, while the general market has {analysis(generalTrend)}"
+    high_data = f"The best performing coin is ${high["base"]} with {analysis(high["trend"])}, while the general market has {analysis(generalTrend)}"
 
 
     twitter.sendTweet(low_data if side == "low" else high_data)
